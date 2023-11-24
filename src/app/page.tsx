a093c43e8be5685e268f0ea7627c4806e2cbb5f4
HarrisAsync <42229360+HarrisAsync@users.noreply.github.com>
2023-11-24 16:58:57 +1100

    deploy

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,34 +3,19 @@ import Image from 'next/image';
 import Link from 'next/link';
 import { MdOutlineArrowForwardIos } from 'react-icons/md';
 
+
+
 export default function Home() {
   return (
     <main>
-      <div className='flex flex-col items-center justify-center p-10 h-screen'>
+      <div className='flex flex-col justify-center h-screen'>
         <div className='flex flex-col lg:flex-row items-center justify-center'>
-          <div className='lg:pr-10'>
-            <Image
-                priority
-                src={'/profileImg.jpeg'}
-                width={4032}
-                height={3024}
-                alt='Profile image'
-              />
-            <div className='w-44 lg:w-60 h-44 lg:h-60'>
-              <Image
-                priority
-                src={'/profileImg.jpeg'}
-                width={4032}
-                height={3024}
-                alt='Profile image'
-                className='object-cover w-full h-full rounded-full'
-              />
-            </div>
-          </div>
+
           <h1 className='pt-5 lg:pt-0 font-mono text-2xl text-center lg:text-left'>
             Hey, I&apos;m a computer science graduate!
           </h1>
         </div>
+
         <Link href='/portfolio' className='hover:text-teal-600 py-10 flex flex-row items-center justify-center'>
           <h1 className='font-mono text-xl pr-1'>
             See my work
