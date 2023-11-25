807d86ac59a14cd40d2258be87b61e5dbebbf20c
HarrisAsync <42229360+HarrisAsync@users.noreply.github.com>
2023-11-25 15:39:01 +1100

    deploy8

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,14 +3,12 @@ import Image from 'next/image';
 import Link from 'next/link';
 import { MdOutlineArrowForwardIos } from 'react-icons/md';
 
-
-
 export default function Home() {
   return (
     <main>
       <div className='flex flex-col justify-center h-screen'>
-        <div className='flex flex-col lg:flex-row items-center justify-center'>
-          <div className='lg:pr-10'>
+        <div className='flex flex-col items-center justify-center'>
+          <div>
             <div className='w-44 lg:w-60 h-44 lg:h-60'>
               <Image
                 priority
@@ -23,14 +21,14 @@ export default function Home() {
             </div>
           </div>
 
-          <h1 className='pt-5 lg:pt-0 font-mono text-2xl text-center lg:text-left'>
-            Hey, I&apos;m a computer science graduate!
+          <h1 className='pt-5 font-mono text-2xl text-center lg:text-left'>
+            Hey, I&apos;m a mathematics & computer science graduate!
           </h1>
         </div>
-
+        
         <Link href='/portfolio' className='hover:text-teal-600 py-10 flex flex-row items-center justify-center'>
           <h1 className='font-mono text-xl pr-1'>
-            See my work
+            See my portfolio
           </h1>
           <MdOutlineArrowForwardIos />
         </Link>
